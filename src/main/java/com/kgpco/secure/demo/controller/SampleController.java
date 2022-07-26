77ce987346191fc6039878059c886bbaa4f39770
C4TECH_PREM\c4tec <premanshu.kumar@kgpco.com>
2022-07-26 21:22:31 +0530

    second commit

--- src/main/java/com/kgpco/secure/demo/controller/SampleController.java
+++ src/main/java/com/kgpco/secure/demo/controller/SampleController.java
@@ -3,8 +3,12 @@ package com.kgpco.secure.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +26,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RestController
 public class SampleController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
 	@Autowired
 	private SampleService sampleService;
@@ -45,6 +51,22 @@ public class SampleController {
 	@PostMapping(value = "/employee", consumes = { "application/xml" })
 	public Employee addEmployee(@RequestBody Employee employee) {
 		return repo.save(employee);
+	}
+
+	@GetMapping("/data")
+	public String getDetails() {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("my-beans.xml");
+
+		SimpleDriverDataSource dataSource = (SimpleDriverDataSource) ctx.getBean("dataSource");
+
+		String detil = "Url: {}" + dataSource.getUrl() + " User name: {}" + dataSource.getUsername() + " Password: {}"
+				+ dataSource.getPassword();
+
+		logger.info("Url: {}", dataSource.getUrl());
+		logger.info("User name: {}", dataSource.getUsername());
+		logger.info(" Password: {}" + dataSource.getPassword());
+
+		return detil;
 	}
 
 }
